be212fc1d9aebf8f10ea1b0ba34487d5e6fab028
Andrea Robu <138387838+andrea-19d@users.noreply.github.com>
2025-09-08 19:10:53 +0300

    created the first HW Assignement v1

---
+++ HW-Assignment2/LowComplexity/Book.cs
@@ -0,0 +1,34 @@
+﻿namespace internship_home_assignment.HW_Assignment2.LowComplexity;
+
+public class Book
+{
+    string Title { get; set; }
+    string Author { get; set; }
+    private int Pages { get; set; }
+
+    public Book(string title, string author, int pages)
+    {
+        Title = title;
+        Author = author;
+        Pages = pages;
+    }
+
+    public static Book CreateBook()
+    {
+        Console.Write($"Enter the book title: ");
+        string title = Console.ReadLine();
+        Console.Write($"Enter the book author: ");
+        string author = Console.ReadLine();
+        Console.Write($"Enter the book pages: ");
+        int pages = int.Parse(Console.ReadLine());
+        
+        Book book = new Book(title, author, pages);
+        return book;
+    }
+
+
+    public void DisplayInfo()
+    {
+        Console.WriteLine($"Title: {Title},  Author: {Author}, Pages: {Pages}");
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using internship_home_assignment.HW_Assignment2.LowComplexity;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        var book = Book.CreateBook();
+        book.DisplayInfo();
+    }
+}
